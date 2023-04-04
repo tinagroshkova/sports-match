a4f874287a39778a604667f8ef018ed415aa1193
Tina <tinagroshkova@gmail.com>
2023-04-04 19:31:31 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          God wish us luck!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Project made by Tina and Andrey
         </a>
       </header>
     </div>
