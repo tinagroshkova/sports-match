f509b2f930cd75df85aaafa7f482d455c3b1833d
Tina <tinagroshkova@gmail.com>
2023-04-06 14:36:28 +0300

    added navbar component

--- src/styleVariables.js
+++ src/styleVariables.js
@@ -2,6 +2,7 @@ export const COLORS = {
     mainColor: "rgb(45, 50, 80)",
     mediumMainColor: "rgb(67, 70, 105)",
     lighterMainColor: "rgb(100, 111, 157)",
+    almostWhiteColor: "rgb(188, 193, 211)",
     contrastColor: "rgb(248, 175, 120)",
     boxShadow: "0 8px 6px -6px black"
   };
