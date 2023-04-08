a1f2cbae15fcf4023491dcd6bc29ee89fb2c6a79
Andrei Alexandrov <andrei.alxv@gmail.com>
2023-04-08 18:58:36 +0300

    another fix in login/register

--- src/pages/RegistrationForm.js
+++ src/pages/RegistrationForm.js
@@ -54,7 +54,7 @@ const RegistrationForm = () => {
         setErrors({});
       } catch (error) {
         alert(error);
-        alert('Registration failed');
+        // alert('Registration failed');
       }
     } else {
       setErrors(errors);
@@ -113,129 +113,19 @@ const RegistrationForm = () => {
             <label>Confirm Password</label>
           </div>
         </Form.Group>
+
+        {/* Премествам тук ерорите, за да излизат най-отдолу */}
+        <Form.Control.Feedback className="text-danger" type="invalid">{errors.username}</Form.Control.Feedback>
+        <Form.Control.Feedback className="text-danger" type="invalid">{errors.confirmPassword}</Form.Control.Feedback>
         <Form.Control.Feedback className="text-danger" type="invalid">{errors.password}</Form.Control.Feedback>
+        
         <Button className="btn-primary" type="submit">Register</Button>
           <div className="registerLink">
-             <p className='haveAnAcount'>Already have an account? <a className="registerRedirect" href="">
-             <Link to="/login">Login</Link></a></p>
+             <p className='haveAnAcount'>Already have an account? 
+             <Link to="/login"><span className='registerHover'> Login</span></Link></p>
           </div>
-
       </form>
     </section>
   );
 }
 export default RegistrationForm;
-
-
-
-
-
-// ---------------------------------------
-
-
-// import React from 'react';
-// import { Form } from 'react-bootstrap';
-// import '../components/LoginAndRegister.css';
-
-// class RegistrationForm extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       username: '',
-//       password: '',
-//       confirmPassword: '',
-//       errors: {}
-//     };
-//   }
-
-//   handleChange = (e) => {
-//     this.setState({ [e.target.name]: e.target.value });
-//   };
-
-//   handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     const { username, password, confirmPassword } = this.state;
-//     const errors = {};
-
-//     if (!username) {
-//       errors.username = 'Username is required';
-//     }
-
-//     if (!password) {
-//       errors.password = 'Password is required';
-//     } else if (password.length < 6) {
-//       errors.password = 'Password must be at least 6 characters long';
-//     } else if (!/\d/.test(password)) {
-//       errors.password = 'Password must contain at least one number';
-//     } else if (!/[A-Z]/.test(password)) {
-//       errors.password = 'Password must contain at least one uppercase letter';
-//     }
-
-//     if (confirmPassword !== password) {
-//       errors.confirmPassword = 'Passwords do not match';
-//     }
-
-//     if (Object.keys(errors).length === 0) {
-//       // Proceed with registration logic
-//       const registrationData = { username, password };
-//       const registrations = JSON.parse(localStorage.getItem('registrations')) || [];
-//       localStorage.setItem('registrations', JSON.stringify([...registrations, registrationData]));
-
-//       alert('Registration successful!');
-//       this.setState({
-//         username: '',
-//         password: '',
-//         confirmPassword: '',
-//         errors: {}
-//       });
-//     } else {
-//       this.setState({ errors });
-//     }
-//   };
-
-//   render() {
-//     const { username, password, confirmPassword, errors } = this.state;
-
-//     return (
-
-//       <section>
-//               <form className="registrationForm" onSubmit={this.handleSubmit}>
-//                 <h2 className="registerTitle">Register</h2>
-//                 <div className="inputBox">
-//                   <span className="icon"><ion-icon name="person"></ion-icon></span>
-//                   <input type="text" name="username" value={username} required onChange={this.handleChange}/>
-//                   <label>Username</label>
-//                 </div>
-                
-//                 {errors.username && <Form.Text className="text-danger">{errors.username}</Form.Text>}
-
-//                 <div className="inputBox">
-//                   <span className="icon"><ion-icon name="lock-closed"></ion-icon></span>
-//                   <input type="password" name="password" value={password} required onChange={this.handleChange}/>
-//                   <label>Password</label>
-//                 </div>
-
-//                 {errors.password && <Form.Text className="text-danger">{errors.password}</Form.Text>}
-
-//                  <div className="inputBox">
-//                   <span className="icon"><ion-icon name="lock-closed"></ion-icon></span>
-//                   <input type="password" name="confirmPassword" value={confirmPassword} required onChange={this.handleChange}/>
-//                   <label>Confirm Password</label>
-//                 </div>
-
-//                 {errors.confirmPassword && <Form.Text className="text-danger">{errors.confirmPassword}</Form.Text>}
-
-//                 <button className="btn-primary" type="submit">Register</button>
-                
-//                 <div className="registerLink">
-//                   <p className='haveAnAcount'>Already have an account? <a className="registerRedirect" href="#"><span
-//                     className="registerHover">Login</span></a></p>
-//                 </div>
-//               </form>
-//           </section>
-//     );
-//   }
-// }
-
-// export default RegistrationForm;
