d9a8d6bdf30091754d61bfc56904e581dee112c6
Tina <tinagroshkova@gmail.com>
2023-04-09 21:43:50 +0300

    added mixin and some activities at page

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,6 +1,9 @@
+import "./Activity.scss";
+
+
 function Activity({ activity, onAdd }) {
     return (
-        <div>
+        <div className="activityContainer">
             <h2>{activity.name}</h2>
             <img src={activity.image} alt={activity.name} />
             <button onClick={() => onAdd(activity)}>Add to List</button>
--- src/pages/Activities/Activities.js
+++ src/pages/Activities/Activities.js
@@ -1,32 +1,49 @@
+import Activity from "../../components/Activity/Activity"
+import { useState } from "react";
 
-// import { useState } from "react";
+import tennis from "../../images/tennis.png";
+import tableTennis  from "../../images/tableTennis.png";
+import football from "../../images/football.png";
 
 export default function ActivitiesPage() {
+    const [addedActivities, setAddedActivities] = useState([]);
+    const activities = [
+        {
+            name: "Tennis",
+            image: tennis,
+            added: false
+        },
+        {
+            name: "Table tennis",
+            image: tableTennis,
+            added: false
+        },
+        {
+            name: "Badminton",
+            image: "https://example.com/badminton.jpg",
+            added: false
+        },
+        {
+            name: "Football",
+            image: football,
+            added: false
+        },
+        // Add the rest of the activities here
+    ];
+
+    function handleAddActivity(activity) {
+        // Implement the logic to add the activity to a list
+        setAddedActivities([...addedActivities, activity]);
+    }
 
     return (
-        <h2>This is the activity page</h2>
-    )
+        <div>
+            {activities.map(activity => (
+                <Activity key={activity.name} activity={activity} onAdd={handleAddActivity} />
+            ))}
+        </div>
+    );
 }
-
-// export default function ActivitiesPage() {
-//     const [addedActivities, setAddedActivities] = useState([]);
-
-//     function handleAddActivity(activity) {
-//         setAddedActivities([...addedActivities, activity]);
-//     }
-
-//     return (
-//         <div>
-//             <h2>Activities</h2>
-//             <div className="activities-list">
-//                 {activities.map((activity) => (
-//                     <Activity
-//                         key={activity.name}
-//                         activity={activity}
-//                         onAdd={handleAddActivity}
-//                     />
-//                 ))}
-//             </div>
 //             <h1>My List</h1>
 //             <div className="added-activities-list">
 //                 {addedActivities.map((activity) => (
@@ -39,6 +56,11 @@ export default function ActivitiesPage() {
 //         </div>
 //     );
 // }
+
+
+
+
+
 
 
 
