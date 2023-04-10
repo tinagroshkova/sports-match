4f8828eb58b038ac081a01e84aa8a549f25d0c35
Tina <tinagroshkova@gmail.com>
2023-04-10 15:31:52 +0300

    fineshed activities container

--- src/pages/Activities/Activities.js
+++ src/pages/Activities/Activities.js
@@ -6,12 +6,19 @@ import tableTennis  from "../../images/activitiesPage/tableTennis.png";
 import football from "../../images/activitiesPage/football.png";
 import badminton from "../../images/activitiesPage/badminton.png";
 import squash from "../../images/activitiesPage/squash.png";
-import running from "../../images/running.png";
+import running from "../../images/activitiesPage/running.png";
 import basketball from "../../images/activitiesPage/basketball.png";
 import volleyball from "../../images/activitiesPage/volleyball.png";
 import ski from "../../images/activitiesPage/ski.png";
 import snowboard from "../../images/activitiesPage/snowboard.png";
 import iceSkating from "../../images/activitiesPage/iceSkating.png";
+import padel from "../../images/activitiesPage/padel.png";
+import wallClimbing from "../../images/activitiesPage/wallClimbing.png";
+import darts from "../../images/activitiesPage/darts.png";
+import paintball from "../../images/activitiesPage/paintball.png";
+import billiards from "../../images/activitiesPage/billiards.png";
+import bowling from "../../images/activitiesPage/bowling.png";
+import karting from "../../images/activitiesPage/karting.png";
 
 
 
@@ -71,6 +78,41 @@ export default function ActivitiesPage() {
         {
             name: "Ice skating",
             image: iceSkating,
+            added: false
+        },
+        {
+            name: "Padel",
+            image: padel,
+            added: false
+        },
+        {
+            name: "Wall climbing",
+            image: wallClimbing,
+            added: false
+        },
+        {
+            name: "Darts",
+            image: darts,
+            added: false
+        },
+        {
+            name: "Paintball",
+            image: paintball,
+            added: false
+        },
+        {
+            name: "Billiards",
+            image: billiards,
+            added: false
+        },
+        {
+            name: "Bowling",
+            image: bowling,
+            added: false
+        },
+        {
+            name: "Karting",
+            image: karting,
             added: false
         },
     ];
