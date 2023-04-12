112cde73d6c1ffdd4e6bedc3c91f5976bcc48b1d
Tina <tinagroshkova@gmail.com>
2023-04-12 20:05:44 +0300

    fixed user editing option

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import userManager from '../../services/UserManager';
 import ActivityComponent from '../../components/Activity/Activity';
-import { User } from '../../services/UserManager';
 import "./Profile.scss";
 
 function ProfilePage() {
-  console.log('userManager instance:', userManager);
   const [user, setUser] = useState(userManager.getLoggedInUser());
   const [isEditing, setIsEditing] = useState(false);
+  const [profileImage, setProfileImage] = useState(user.image);
 
   useEffect(() => {
     setUser(userManager.getLoggedInUser());
@@ -27,16 +26,29 @@ function ProfilePage() {
   const handleSave = () => {
     userManager.setLoggedInUser(user);
     setIsEditing(false);
-    console.log(user);
   };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setProfileImage(reader.result);
+      setUser({ ...user, image: reader.result });
+    };
+    reader.readAsDataURL(file);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('loggedInUser', JSON.stringify(user));
+  }, [user]);
 
   return (
     <div className="profilePageContainer">
       <div className="profileInfo">
         <div className="profileImage">
-          <img src={`data:image/png;base64,${user.image}`} alt={user.username} />
+          <img src={profileImage} alt={user.username} />
           {isEditing && (
-            <input type="file" name="image" onChange={handleEdit} accept="image/*" />
+            <input type="file" name="image" onChange={handleImageChange} accept="image/*" />
           )}
         </div>
         <h2>
@@ -78,19 +90,18 @@ function ProfilePage() {
         </div>
         {isEditing ? (
           <button onClick={handleSave}>Save</button>
-          
         ) : (
           <button onClick={() => setIsEditing(true)}>Edit</button>
         )}
       </div>
 
-      <div className="activitiesList">
+      <div>
         <h3>My Activities:</h3>
         {user && user.activities && user.activities.length > 0 ? (
-          <div>
+          <div className="activitiesList">
             {user.activities.map((activity) => (
               <div key={activity.name}>
-                <ActivityComponent activity={activity} onRemove={() => handleRemoveActivity(activity)} />
+                <ActivityComponent activity={activity} onRemove={() => handleRemoveActivity(activity)} className="smallActivity"/>
               </div>
             ))}
           </div>
@@ -103,3 +114,109 @@ function ProfilePage() {
 }
 
 export default ProfilePage;
+// import React, { useState, useEffect } from 'react';
+// import userManager from '../../services/UserManager';
+// import ActivityComponent from '../../components/Activity/Activity';
+// import { User } from '../../services/UserManager';
+// import "./Profile.scss";
+
+// function ProfilePage() {
+//   const [user, setUser] = useState(userManager.getLoggedInUser());
+//   const [isEditing, setIsEditing] = useState(false);
+
+//   useEffect(() => {
+//     setUser(userManager.getLoggedInUser());
+//   }, []);
+
+//   const handleRemoveActivity = (activity) => {
+//     const newUser = userManager.getLoggedInUser();
+//     newUser.removeActivity(activity);
+//     localStorage.setItem('loggedInUser', JSON.stringify(newUser));
+//     setUser(newUser);
+//   };
+
+//   const handleEdit = (event) => {
+//     setUser({ ...user, [event.target.name]: event.target.type === 'number' ? parseInt(event.target.value) : event.target.value });
+//   };
+
+//   const handleSave = () => {
+//     userManager.setLoggedInUser(user);
+//     setIsEditing(false);
+//   };
+
+//   useEffect(() => {
+//     localStorage.setItem('loggedInUser', JSON.stringify(user));
+//   }, [user]);
+
+//   return (
+//     <div className="profilePageContainer">
+//       <div className="profileInfo">
+//         <div className="profileImage">
+//           <img src={`data:image/png;base64,${user.image}`} alt={user.username} />
+//           {isEditing && (
+//             <input type="file" name="image" onChange={handleEdit} accept="image/*" />
+//           )}
+//         </div>
+//         <h2>
+//           {isEditing ? (
+//             <input type="text" name="username" value={user.username} onChange={handleEdit} />
+//           ) : (
+//             user.username
+//           )}
+//         </h2>
+//         <div className="userInfo">
+//           <p>
+//             Age:{' '}
+//             {isEditing ? (
+//               <input type="number" name="age" value={user.age} onChange={handleEdit} />
+//             ) : (
+//               user.age
+//             )}
+//           </p>
+//           <p>
+//             City:{' '}
+//             {isEditing ? (
+//               <input type="text" name="city" value={user.city} onChange={handleEdit} />
+//             ) : (
+//               user.city
+//             )}
+//           </p>
+//           <p>
+//             Gender:{' '}
+//             {isEditing ? (
+//               <select name="gender" value={user.gender} onChange={handleEdit}>
+//                 <option value="male">Male</option>
+//                 <option value="female">Female</option>
+//                 <option value="other">Other</option>
+//               </select>
+//             ) : (
+//               user.gender
+//             )}
+//           </p>
+//         </div>
+//         {isEditing ? (
+//           <button onClick={handleSave}>Save</button>
+//         ) : (
+//           <button onClick={() => setIsEditing(true)}>Edit</button>
+//         )}
+//       </div>
+
+//       <div className="activitiesList">
+//         <h3>My Activities:</h3>
+//         {user && user.activities && user.activities.length > 0 ? (
+//           <div>
+//             {user.activities.map((activity) => (
+//               <div key={activity.name}>
+//                 <ActivityComponent activity={activity} onRemove={() => handleRemoveActivity(activity)} />
+//               </div>
+//             ))}
+//           </div>
+//         ) : (
+//           <p>You haven't added any activities yet.</p>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ProfilePage;
--- src/services/UserManager.js
+++ src/services/UserManager.js
@@ -1,12 +1,36 @@
-export class User {
+class User {
   constructor(username, password) {
     this.username = username;
     this.password = password;
-    this.image = null;
-    this.age = null;
-    this.city = null;
-    this.gender = null;
+    this.image = "";
+    this.age = "";
+    this.city = "";
+    this.gender = "";
     this.activities = JSON.parse(localStorage.getItem(`${this.username}_activities`)) || [];
+    this.loadFromLocalStorage();
+  }
+
+  loadFromLocalStorage() {
+    const userJson = localStorage.getItem("loggedInUser");
+    if (userJson) {
+      const userObj = JSON.parse(userJson);
+      this.username = userObj.username;
+      this.password = userObj.password;
+      this.image = userObj.image || "";
+      this.age = userObj.age || "";
+      this.city = userObj.city || "";
+      this.gender = userObj.gender || "";
+      this.activities = userObj.activities || [];
+    }
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem(`${this.username}_data`, JSON.stringify({
+      image: this.image,
+      age: this.age,
+      city: this.city,
+      gender: this.gender,
+    }));
   }
 
   addActivity(activity) {
@@ -80,8 +104,9 @@ class UserManager {
       loggedInUser.city = user.city;
       loggedInUser.gender = user.gender;
       loggedInUser.image = user.image;
+      loggedInUser.saveToLocalStorage(); // call the saveToLocalStorage method
       localStorage.setItem("loggedInUser", JSON.stringify(loggedInUser));
-
+  
       const users = JSON.parse(localStorage.getItem("users")) || [];
       const updatedUsers = users.map(u => {
         if (u.username === loggedInUser.username) {
@@ -98,4 +123,104 @@ class UserManager {
 const userManager = new UserManager();
 
 export default userManager;
+// export class User {
+//   constructor(username, password) {
+//     this.username = username;
+//     this.password = password;
+//     this.image = "";
+//     this.age = "";
+//     this.city = "";
+//     this.gender = "";
+//     this.activities = JSON.parse(localStorage.getItem(`${this.username}_activities`)) || [];
+//   }
+
+//   addActivity(activity) {
+//     if (!this.hasActivity(activity)) {
+//       this.activities.push(activity);
+//       localStorage.setItem(`${this.username}_activities`, JSON.stringify(this.activities));
+//     }
+//   }
+
+//   removeActivity(activity) {
+//     if (this.hasActivity(activity)) {
+//       this.activities = this.activities.filter(a => a.name !== activity.name);
+//       localStorage.setItem(`${this.username}_activities`, JSON.stringify(this.activities));
+//     }
+//   }
+
+//   hasActivity(activity) {
+//     return this.activities.some(a => a.name === activity.name);
+//   }
+// }
+
+// class UserManager {
+//   constructor() {
+//     const users = JSON.parse(localStorage.getItem("users")) || [];
+//     this.users = users.map((user) => new User(user.username, user.password));
+//   }
+
+//   registerUser = (username, password) => {
+//     const userExists = this.users.some((user) => user.username === username);
+
+//     if (userExists) {
+//       return Promise.reject(new Error("Username already exists"));
+//     }
+
+//     const user = new User(username, password);
+//     this.users.push(user);
+//     localStorage.setItem("users", JSON.stringify(this.users));
+//     return Promise.resolve();
+//   };
+
+//   loginUser = (username, password) => {
+//     const user = this.users.find((user) => user.username === username && user.password === password);
+
+//     if (!user) {
+//       return Promise.reject(new Error("Invalid username or password"));
+//     }
+
+//     localStorage.setItem("loggedInUser", JSON.stringify(user));
+//     return Promise.resolve();
+//   };
+
+//   logoutUser = () => {
+//     localStorage.removeItem("loggedInUser");
+//     return Promise.resolve();
+//   };
+
+//   getLoggedInUser = () => {
+//     const userJson = localStorage.getItem("loggedInUser");
+//     if (!userJson) {
+//       return null;
+//     }
+//     const userObj = JSON.parse(userJson);
+//     return new User(userObj.username, userObj.password);
+//   };
+
+//   setLoggedInUser = (user) => {
+//     const loggedInUser = this.getLoggedInUser();
+//     if (loggedInUser) {
+//       loggedInUser.username = user.username;
+//       loggedInUser.age = user.age;
+//       loggedInUser.city = user.city;
+//       loggedInUser.gender = user.gender;
+//       loggedInUser.image = user.image;
+//       localStorage.setItem("loggedInUser", JSON.stringify(loggedInUser));
+
+//       const users = JSON.parse(localStorage.getItem("users")) || [];
+//       const updatedUsers = users.map(u => {
+//         if (u.username === loggedInUser.username) {
+//           console.log(loggedInUser);
+//           return loggedInUser;
+//         }
+//       });
+//       localStorage.setItem("users", JSON.stringify(updatedUsers));
+//     }
+//   };
+  
+// }
+
+// const userManager = new UserManager();
+
+// export default userManager;
 
