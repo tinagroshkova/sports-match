a1b47cdd82643d41f1393436e96f55e008a9cb7c
Andrei Alexandrov <andrei.alxv@gmail.com>
2023-04-13 10:54:23 +0300

    added functionality to the buddy page

--- src/pages/BuddySearch/BuddySearch.js
+++ src/pages/BuddySearch/BuddySearch.js
@@ -1,6 +1,39 @@
+import React, { useState } from 'react';
+import selectValues from './activities.json';
+import UserManager from '../../services/UserManager';
+
 export default function BuddySearchPage() {
+    const [selectedValue, setSelectedValue] = useState('');
+
+    const handleChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
+
+    // Filter the users array to find users with matching activities
+    const filteredUsers = UserManager.users.filter((user) =>
+        user.activities.some((activity) => activity.name === selectedValue)
+    );
+    console.log("QQQ", UserManager.users)
 
     return (
-        <h2>This is the buddy search page</h2>
-    )
+        <div>
+            <h2>This is the buddy search page</h2>
+            <label htmlFor="activity-select">Select an activity:</label>
+            <select id="activity-select" value={selectedValue} onChange={handleChange}>
+                <option value="">Select an activity</option>
+                {selectValues.map((activity) => (
+                    <option key={activity.type} value={activity.type}>
+                        {activity.type}
+                    </option>
+                ))}
+            </select>
+            {filteredUsers.map((user) => (
+                <div key={user.username}>
+                    <img src={user.image} alt={user.username} />
+                    <h3>{user.username}</h3>
+                    <p>Activities: {user.activities.map((activity) => activity.name).join(', ')}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
