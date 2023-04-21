ba5265bee7fe7c531202efa184472c8f3d12edad
Tina <tinagroshkova@gmail.com>
2023-04-21 12:08:50 +0300

    work on chat page in progress

--- src/pages/Messages/Messages.js
+++ src/pages/Messages/Messages.js
@@ -1,3 +1,166 @@
+// import React, { useState, useEffect, useRef } from "react";
+// import userManager from "../../services/UserManager";
+// import { Message, messagesManager } from "../../services/MessagesManager";
+// import { useNavigate } from "react-router-dom";
+// import "./Messages.scss";
+// import { useLocation } from "react-router-dom";
+
+// const CHAT_STORAGE_KEY = 'chatState';
+
+// const Messages = (props) => {
+//   const location = useLocation();
+//   const [messages, setMessages] = useState([]);
+//   const [receiver, setReceiver] = useState(location.state?.receiver || props.location?.state?.receiver || "")
+//   const navigate = useNavigate();
+//   const messageListRef = useRef(null);
+//   const loggedInUser = userManager.getLoggedInUser();
+//   console.log(localStorage.getItem(CHAT_STORAGE_KEY));
+//   const [conversations, setConversations] = useState([]);
+//   const [selectedConversation, setSelectedConversation] = useState("");
+//   const [newConversation, setNewConversation] = useState("");
+//   // messagesManager.checkStorage();
+
+//   const handleSendMessage = (event, conversation) => {
+//     event.preventDefault();
+//     if (!loggedInUser) {
+//       alert("You have to log in first!");
+//       navigate('/login');
+//       return;
+//     }
+//     const messageText = event.target.message.value;
+//     const message = new Message(messageText, new Date(), loggedInUser.username, conversation);
+//     messagesManager.addMessage(message);
+//     event.target.reset();
+//   };
+
+//   const handleMessageUpdate = () => {
+//     const updatedConversations = messagesManager.getConversations(loggedInUser.username);
+//     setConversations(updatedConversations);
+//   };
+  
+//   const handleNewConversation = (event) => {
+//     event.preventDefault();
+//     if (newConversation && !conversations.includes(newConversation)) {
+//       setConversations([...conversations, newConversation]);
+//     }
+//     setNewConversation("");
+//   };
+
+//   useEffect(() => {
+//     if (!loggedInUser) {
+//       alert("You have to log in first!");
+//       navigate('/login');
+//       return;
+//     }
+
+//     messagesManager.loadMessagesFromStorage();
+//     messagesManager.setOnUpdate(handleMessageUpdate);
+//     setMessages(messagesManager.loadMessagesFromStorage());
+
+//     return () => {
+//       messagesManager.removeOnUpdate(handleMessageUpdate);
+//     };
+//   }, [loggedInUser, location.state?.receiver]);
+
+//   useEffect(() => {
+//     setReceiver(location.state?.receiver || "");
+//   }, [location.state?.receiver]);
+
+//   useEffect(() => {
+//     if (messageListRef.current) {
+//       messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+//     }
+//   }, [messages]);
+
+//   const formatDate = (date) => {
+//     const currentDate = new Date();
+//     const messageDate = new Date(date);
+
+//     let dateString = '';
+//     if (currentDate.getDate() !== messageDate.getDate() ||
+//       currentDate.getMonth() !== messageDate.getMonth() ||
+//       currentDate.getFullYear() !== messageDate.getFullYear()) {
+//       const dateOptions = {
+//         year: 'numeric',
+//         month: 'short',
+//         day: 'numeric',
+//       };
+//       dateString = messageDate.toLocaleString('en-US', dateOptions) + ', ';
+//     }
+
+//     const timeOptions = {
+//       hour: '2-digit',
+//       minute: '2-digit',
+//       hour12: false,
+//     };
+//     const timeString = messageDate.toLocaleString('bg-BG', timeOptions);
+
+//     return dateString + timeString;
+//   }
+//   console.log(loggedInUser.username);
+//   console.log(receiver);
+
+//   return (
+//     <div className="chatPage">
+//       {conversations.map((conversation, index) => (
+//         <Conversation
+//           key={index}
+//           conversation={conversation}
+//           loggedInUser={loggedInUser}
+//           messages={messages}
+//           formatDate={formatDate}
+//           handleSendMessage={handleSendMessage}
+//         />
+//       ))}
+//     </div>
+//   );
+//       }
+
+// const MessageComponent = ({ message, loggedInUser, formatDate }) => {
+//   const isSentByLoggedInUser = message.sender === loggedInUser?.username;
+//   const timestamp = formatDate(message.timestamp);
+
+//   return (
+//     <>
+//       <span className="shortTimestamp">{timestamp}</span>
+//       <li className={`message ${isSentByLoggedInUser ? "sender" : "receiver"}`}>
+//         {message.text}
+//       </li>
+//     </>
+//   );
+// }
+// const Conversation = ({ conversation, loggedInUser, messages, formatDate, handleSendMessage }) => {
+//   const messageListRef = useRef(null);
+
+//   useEffect(() => {
+//     if (messageListRef.current) {
+//       messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+//     }
+//   }, [messages]);
+
+//   return (
+//     <div className="chatContainer">
+//       {loggedInUser && (
+//         <h1>{conversation}</h1>
+//       )}
+//       <div className="messagesWrapper">
+//         <ul className="messagesList" ref={messageListRef}>
+//           {messages
+//             .filter((message) => message.sender === conversation || message.receiver === conversation)
+//             .map((message, index) => (
+//               <MessageComponent key={index} message={message} loggedInUser={loggedInUser} formatDate={formatDate} />
+//             ))}
+//         </ul>
+//         <form className="messagesForm" onSubmit={(e) => handleSendMessage(e, conversation)}>
+//           <input type="text" name="message" placeholder="Type a message..." autoComplete="off" />
+//           <button type="submit">Send</button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Messages;
 import React, { useState, useEffect, useRef } from "react";
 import userManager from "../../services/UserManager";
 import { Message, messagesManager } from "../../services/MessagesManager";
@@ -5,13 +168,18 @@ import { useNavigate } from "react-router-dom";
 import "./Messages.scss";
 import { useLocation } from "react-router-dom";
 
+const CHAT_STORAGE_KEY = 'chatState';
+
 const Messages = (props) => {
   const location = useLocation();
   const [messages, setMessages] = useState([]);
   const [receiver, setReceiver] = useState(location.state?.receiver || props.location?.state?.receiver || "")
   const navigate = useNavigate();
+  const [conversations, setConversations] = useState([]);
   const messageListRef = useRef(null);
   const loggedInUser = userManager.getLoggedInUser();
+  console.log(localStorage.getItem(CHAT_STORAGE_KEY));
+  // messagesManager.checkStorage();
 
   const handleSendMessage = (event) => {
     event.preventDefault();
@@ -29,6 +197,11 @@ const Messages = (props) => {
   const handleMessageUpdate = () => {
     setMessages(messagesManager.getMessagesByReceiver(receiver));
   };
+  useEffect(() => {
+    // Get the unique receivers from the messages
+    const uniqueReceivers = [...new Set(messages.map((message) => message.receiver))];
+    setConversations(uniqueReceivers);
+  }, [messages]);
 
   useEffect(() => {
     if (!loggedInUser) {
@@ -39,6 +212,7 @@ const Messages = (props) => {
 
     messagesManager.loadMessagesFromStorage();
     messagesManager.setOnUpdate(handleMessageUpdate);
+    setMessages(messagesManager.loadMessagesFromStorage());
 
     return () => {
       messagesManager.removeOnUpdate(handleMessageUpdate);
@@ -80,6 +254,8 @@ const Messages = (props) => {
 
     return dateString + timeString;
   }
+  console.log(loggedInUser.username);
+  console.log(receiver);
 
   return (
     <div className="chatPage">
@@ -89,9 +265,15 @@ const Messages = (props) => {
         )}
         <div className="messagesWrapper">
           <ul className="messagesList" ref={messageListRef}>
-            {messages.map((message, index) => (
-              <MessageComponent key={index} message={message} loggedInUser={loggedInUser} formatDate={formatDate} />
-            ))}
+            {messages
+              .filter(
+                (message) =>
+                  (message.sender === loggedInUser.username && message.receiver === receiver) ||
+                  (message.sender === receiver && message.receiver === loggedInUser.username)
+              )
+              .map((message, index) => (
+                <MessageComponent key={index} message={message} loggedInUser={loggedInUser} formatDate={formatDate} />
+              ))}
           </ul>
 
           <form className="messagesForm" onSubmit={handleSendMessage}>
@@ -102,7 +284,8 @@ const Messages = (props) => {
       </div>
     </div>
   );
-}
+};
+
 
 const MessageComponent = ({ message, loggedInUser, formatDate }) => {
   const isSentByLoggedInUser = message.sender === loggedInUser?.username;
@@ -117,6 +300,39 @@ const MessageComponent = ({ message, loggedInUser, formatDate }) => {
     </>
   );
 }
+const Conversation = ({ receiver, loggedInUser, formatDate, messages, handleSendMessage }) => {
+  const messageListRef = useRef(null);
+
+  const filteredMessages = messages.filter(
+    (message) =>
+      (message.sender === receiver && message.receiver === loggedInUser.username) ||
+      (message.sender === loggedInUser.username && message.receiver === receiver)
+  );
+
+  useEffect(() => {
+    if (messageListRef.current) {
+      messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+    }
+  }, [filteredMessages]);
+
+  return (
+    <div className="chatContainer">
+      {loggedInUser && <h1>{receiver ? receiver : "nobody"}</h1>}
+      <div className="messagesWrapper">
+        <ul className="messagesList" ref={messageListRef}>
+          {filteredMessages.map((message, index) => (
+            <MessageComponent key={index} message={message} loggedInUser={loggedInUser} formatDate={formatDate} />
+          ))}
+        </ul>
+
+        <form className="messagesForm" onSubmit={handleSendMessage}>
+          <input type="text" name="message" placeholder="Type a message..." autoComplete="off" />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 
 export default Messages;
--- src/services/MessagesManager.js
+++ src/services/MessagesManager.js
@@ -1,3 +1,99 @@
+// import userManager from "./UserManager";
+
+// class Message {
+//   constructor(text, timestamp, sender, receiver) {
+//     this.text = text;
+//     this.timestamp = timestamp;
+//     this.sender = sender;
+//     this.receiver = receiver;
+//     this.read = false;
+//   }
+// }
+
+// const CHAT_STORAGE_KEY = 'chatState';
+
+// class MessagesManager {
+//   constructor() {
+//     this.messages = [];
+//     this.onUpdateCallbacks = [];
+//     this.intervalId = null;
+//     this.startCheckingStorage();
+//   }
+
+//   loadMessagesFromStorage() {
+//     const storedMessages = JSON.parse(localStorage.getItem(CHAT_STORAGE_KEY)) || [];
+//     if (Array.isArray(storedMessages)) {
+//       console.log(storedMessages);
+//       return this.messages = storedMessages.map(message => new Message(message.text, new Date(message.timestamp), message.sender, message.receiver));
+//       // console.log(this.messages);
+//     } else {
+//       this.messages = [];
+//     }
+//   }
+
+//   addMessage(message) {
+//     this.messages.push(message);
+//     this.saveMessagesToStorage();
+//     this.onUpdateCallbacks.forEach(callback => callback());
+//   }
+
+//   saveMessagesToStorage() {
+//     localStorage.setItem(CHAT_STORAGE_KEY, JSON.stringify(this.messages));
+//   }
+//   getMessagesByReceiver(receiver) {
+//     const loggedInUser = userManager.getLoggedInUser();
+//     if (!loggedInUser) {
+//       return [];
+//     }
+//     return this.messages.filter(message =>
+//       (message.sender === receiver && message.receiver === loggedInUser.username) ||
+//       (message.sender === loggedInUser.username && message.receiver === receiver)
+//     );
+//   }
+
+//   getConversations(username) {
+//     const conversationSet = new Set();
+//     this.messages.forEach(message => {
+//       if (message.sender === username) {
+//         conversationSet.add(message.receiver);
+//       } else if (message.receiver === username) {
+//         conversationSet.add(message.sender);
+//       }
+//     });
+//     return Array.from(conversationSet);
+//   }
+
+
+//   checkStorage() {
+//     const storedMessages = JSON.parse(localStorage.getItem(CHAT_STORAGE_KEY));
+//     if (storedMessages && Array.isArray(storedMessages)) {
+//       const newMessages = storedMessages.filter(message => !this.messages.some(m => new Date(m.timestamp).toString() === new Date(message.timestamp).toString()));
+//       if (newMessages.length > 0) {
+//         this.messages.push(...newMessages.map(m => new Message(m.text, new Date(m.timestamp), m.sender, m.receiver)));
+//         this.onUpdateCallbacks.forEach(callback => callback());
+//       }
+//     }
+//   }
+
+//   setOnUpdate(onUpdate) {
+//     this.onUpdateCallbacks.push(onUpdate);
+//   }
+//   removeOnUpdate(callback) {
+//     const index = this.onUpdateCallbacks.indexOf(callback);
+//     if (index !== -1) {
+//       this.onUpdateCallbacks.splice(index, 1);
+//     }
+//   }
+
+//   startCheckingStorage() {
+//     this.intervalId = setInterval(() => this.checkStorage(), 5000);
+//   }
+  
+// }
+
+// const messagesManager = new MessagesManager();
+// export { Message, messagesManager };
+
 import userManager from "./UserManager";
 
 class Message {
@@ -6,6 +102,7 @@ class Message {
     this.timestamp = timestamp;
     this.sender = sender;
     this.receiver = receiver;
+    this.read = false;
   }
 }
 
@@ -22,7 +119,9 @@ class MessagesManager {
   loadMessagesFromStorage() {
     const storedMessages = JSON.parse(localStorage.getItem(CHAT_STORAGE_KEY)) || [];
     if (Array.isArray(storedMessages)) {
-      this.messages = storedMessages.map(message => new Message(message.text, new Date(message.timestamp), message.sender, message.receiver));
+      console.log(storedMessages);
+      return this.messages = storedMessages.map(message => new Message(message.text, new Date(message.timestamp), message.sender, message.receiver));
+      // console.log(this.messages);
     } else {
       this.messages = [];
     }
@@ -39,7 +138,6 @@ class MessagesManager {
     if (!loggedInUser) {
       return [];
     }
-
     return this.messages.filter(message =>
       (message.sender === receiver && message.receiver === loggedInUser.username) ||
       (message.sender === loggedInUser.username && message.receiver === receiver)
@@ -59,6 +157,17 @@ class MessagesManager {
         this.onUpdateCallbacks.forEach(callback => callback());
       }
     }
+  }
+  getConversations(username) {
+    const conversationSet = new Set();
+    this.messages.forEach(message => {
+      if (message.sender === username) {
+        conversationSet.add(message.receiver);
+      } else if (message.receiver === username) {
+        conversationSet.add(message.sender);
+      }
+    });
+    return Array.from(conversationSet);
   }
 
   setOnUpdate(onUpdate) {
