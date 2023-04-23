57d8ffba6853c3ba5409c0b9833a102c868ef35c
Tina <tinagroshkova@gmail.com>
2023-04-23 14:02:50 +0300

    fixed duplicating messages at chat page/ added styles at messages component

--- src/pages/BuddySearch/BuddySearch.js
+++ src/pages/BuddySearch/BuddySearch.js
@@ -1,3 +1,89 @@
+// import React, { useState, useEffect } from "react";
+// import userManager from "../../services/UserManager";
+// import "./BuddySearch.scss";
+// import { useNavigate } from "react-router-dom";
+// import userImage from "../../images/user.png";
+// import BuddyCard from "../../components/BuddyCard/BuddyCard";
+// import activitiesData from "../Activities/activitiesData";
+// import swal from "sweetalert2";
+
+// export default function BuddySearchPage() {
+//   const [users, setUsers] = useState(userManager.users);
+//   const [selectedValue, setSelectedValue] = useState("");
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const checkLoggedInUser = async () => {
+//       const loggedInUser = userManager.getLoggedInUser();
+//       if (!loggedInUser) {
+//         const alertResult = await swal.fire({
+//           title: 'You are not logged in!',
+//           text: 'You need to be logged in to use Buddy Search.',
+//           icon: 'warning',
+//           confirmButtonText: 'OK',
+//         });
+
+//         if (alertResult.isConfirmed) {
+//           navigate('/login');
+//           return;
+//         }
+//       }
+//       setUsers(userManager.users);
+//     };
+
+//     checkLoggedInUser();
+//   }, []);
+
+//   useEffect(() => {
+//     const loggedInUser = userManager.getLoggedInUser();
+//     if (!loggedInUser) {
+//       // alert("You have to log in first!");
+//       // navigate('/login');
+//       // return;
+//     }
+//     setUsers(userManager.users);
+//   }, []);
+
+//   useEffect(() => {
+//     setUsers(userManager.users);
+//   }, [userManager.getLoggedInUser()?.activities]);
+
+//   const handleChange = (event) => {
+//     setSelectedValue(event.target.value);
+//   };
+
+//   // Filter the users array to find users with matching activities
+//   const filteredUsers = users.filter(
+//     (user) =>
+//       user.username !== userManager.getLoggedInUser()?.username &&
+//       user.activities.some((activity) => activity.name === selectedValue)
+//   );
+
+//   const handleStartChat = (otherUser) => { navigate('/messages', { state: { receiver: otherUser.username } }); }
+
+//   return (
+//     <div className="buddyPage">
+//       <h2 className="siteSloganTitle">Find someone that shares your sport passion</h2>
+//       <div className="buddySearchWrapper">
+//         <select className='buddySearchSelect' id="activity-select" value={selectedValue} onChange={handleChange}>
+//           <option value="">Search buddy by activity</option>
+//           {activitiesData.sort((a, b) => a.name.localeCompare(b.name)).map((activity) => (
+//             <option key={activity.name} value={activity.name}>
+//               {activity.name}
+//             </option>
+//           ))}
+//         </select>
+//       </div>
+//       <div className="buddiesHolder">
+//         {filteredUsers.map((user) => (
+//           <div className="buddyCardContainer" key={user.username}>
+//             <BuddyCard user={user} defaultImage={userImage} onStartChat={() => handleStartChat(user)} />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
 import React, { useState, useEffect } from "react";
 import userManager from "../../services/UserManager";
 import "./BuddySearch.scss";
@@ -48,6 +134,7 @@ export default function BuddySearchPage() {
   const handleStartChat = (otherUser) => {
     navigate("/messages", { state: { receiver: otherUser.username } });
   };
+
 
   return (
     <div className="buddyPage">
