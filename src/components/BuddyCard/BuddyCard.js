66980b75b90d3e627f297818ac4af1f8764810a7
Andrei Alexandrov <andrei.alxv@gmail.com>
2023-04-24 13:22:32 +0300

    new cards in buddies page

--- src/components/BuddyCard/BuddyCard.js
+++ src/components/BuddyCard/BuddyCard.js
@@ -6,13 +6,24 @@ export default function BuddyCard(props) {
   const { user, onStartChat } = props;
 
   return (
-    <div className="buddyCardContainer">
-      <div className="buddyCard">
+    <div className="box">
+      <div className="imgBx">
         <img src={user.image || userImage} alt={user.username} />
-        <h3>{user.username}</h3>
-        <p>Favourite activities: {user.activities.map((activity) => activity.name).join(", ")}</p>
-        <button className="chatBtn" onClick={() => onStartChat(user)}>Start Chat</button>
+      </div>
+      <div className="content">
+        <h3>{user.username} <br></br>
+          <span>Favourite activities: {user.activities.map((activity) => activity.name).join(", ")} <br></br></span>
+          <button className="chatBtn" onClick={() => onStartChat(user)}>Start Chat</button></h3>
       </div>
     </div>
   );
 }
+
+{/* <div className="buddyCardContainer">
+<div className="buddyCard">
+  <img src={user.image || userImage} alt={user.username} />
+  <h3>{user.username}</h3>
+  <p>Favourite activities: {user.activities.map((activity) => activity.name).join(", ")}</p>
+  <button className="chatBtn" onClick={() => onStartChat(user)}>Start Chat</button>
+</div>
+</div> */}
